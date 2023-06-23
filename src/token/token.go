05e74b2ab95f1814c0253acb3c06078f9b636435
Vitaly Senokosov <vitalyse@playtika.com>
2023-06-23 12:10:08 +0300

    add !/*<> tokens

--- src/token/token.go
+++ src/token/token.go
@@ -15,8 +15,14 @@ const (
 	INT   = "INT"   // 1343456
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
 
 	// Delimiters
 	COMMA     = ","

3e1dab067101feb72c7ceaa4efefc89646516294
Vitaly Senokosov <6545415@gmail.com>
2025-01-26 23:35:18 +0300

    start Ast implementation

---
+++ src/JuLang/JuLang/Ast/Ast.cs
@@ -0,0 +1,21 @@
+﻿namespace JuLang;
+
+public interface INode {
+    string TokenLiteral();
+}
+
+public interface IStatement : INode {
+    void StatementNode();
+}
+
+public interface IExpression : INode {
+    void ExpressionNode();
+}
+
+public class Program : INode {
+    public List<IStatement> Statements { get; set; } = new List<IStatement>();
+
+    public string TokenLiteral() {
+        return Statements.Count > 0 ? Statements[0].TokenLiteral() : string.Empty;
+    }
+}
